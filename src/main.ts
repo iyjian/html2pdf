90f9b3fd40aeb5e694c737e354329f5cd3783cf0
w.c <ubuntu@wcsnapshot.cloud.onecloud.io>
2024-04-30 05:45:59 +0000

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
+
   // app.useStaticAssets(join(__dirname, '..', 'public'));
   // app.setBaseViewsDir(join(__dirname, '..', 'views'));
   // app.setViewEngine('hbs');
